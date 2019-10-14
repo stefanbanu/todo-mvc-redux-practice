a2ada1f88de76c5e32b408f01e8bce9ab30b1ba7
Stefan Banu <stefanbanu1@gmail.com>
2019-10-14 07:07:40 +0300

    work in progress

--- view.js
+++ view.js
@@ -1,33 +1,62 @@
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
+const input = document.querySelector(".new-todo");
+const ul = document.querySelector(".todo-list");
 
-input.addEventListener('keydown', (event) => {
-  if (event.keyCode !== 13) {
-    return;
+document.querySelector(".new-todo").addEventListener("keydown", event => {
+  if (event.key.toLocaleLowerCase() === "enter" && input.value) {
+    const action = actions.addTodo(event.target.value);
+    window.store.dispatch(action);
+    input.value = "";
   }
-
-  const value = event.target.value.trim();
-
-  if (!value) {
-    return;
-  }
-
-  const action = actions.addTodo(value);
-
-  store.dispatch(action);
-
-  event.target.value = '';
 });
 
-store.subscribe((latestState) => {
+store.subscribe(latestState => {
   console.log(latestState);
-  ul.innerHTML = '';
+  ul.innerHTML = "";
 
-  latestState.forEach((todo) => {
-    const li = document.createElement('li');
+  latestState.forEach(todo => {
+    const item = document.createElement("li");
+    // li.innerHTML = todo.text;
 
-    li.innerHTML = todo.text;
+    let divView = document.createElement("div");
+    divView.classList.add("view");
 
-    ul.appendChild(li);
+    let checkBox = document.createElement("input");
+    checkBox.classList.add("toggle");
+    checkBox.setAttribute("type", "checkbox");
+
+    checkBox.addEventListener('change', completedItem);
+
+    divView.appendChild(checkBox);
+
+    let label = document.createElement("Label");
+    label.innerHTML = input.value;
+
+    divView.appendChild(label);
+
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("destroy");
+    removeButton.addEventListener("click", removeItem);
+
+    divView.appendChild(removeButton);
+
+    item.appendChild(divView);
+
+    ul.appendChild(item);
+    // ul.insertBefore(item, ul.childNodes[0]);
   });
 });
+
+function completedItem(checked){
+  let item = this.parentNode.parentNode;
+  if (checked) {
+    item.classList.add("completed");
+  } else {
+    item.classList.remove("completed");
+  }
+}
+
+function removeItem() {
+  let item = this.parentNode.parentNode;
+  let parent = item.parentNode;
+  parent.removeChild(item);
+}
